b6cdc6a28baf3b5728069285f1f18a61f8224ada
nicolasbelfis <nicolas.belfis@codurance.com>
2020-10-18 18:31:40 +0200

    final solution

--- src/main/scala/example/PrimeFactor.scala
+++ src/main/scala/example/PrimeFactor.scala
@@ -1,7 +1,15 @@
 package example
 
-object PrimeFactor {
+object PrimeFactors {
 
-  def of(number: Int): List[Int] = ???
+  def of(number: Int): List[Int] = {
+    if(number > 1) {
+      divide(number,2)
+    } else List()
+  }
 
+  private def divide(number: Int, divisor : Int): List[Int] = {
+    if (number % divisor == 0) divisor :: of(number / divisor)
+    else divide(number,divisor+1)
+  }
 }
--- src/test/scala/example/PrimeFactorSpec.scala
+++ src/test/scala/example/PrimeFactorSpec.scala
@@ -5,6 +5,16 @@ import org.scalatest.matchers.should.Matchers
 
 class PrimeFactorSpec extends AnyFlatSpec with Matchers {
   "The Prime factor object" should "return a list of prime factors" in {
-    PrimeFactor.of(1) shouldBe List()
+    PrimeFactors.of(1) shouldBe List()
+    PrimeFactors.of(2) shouldBe List(2)
+    PrimeFactors.of(3) shouldBe List(3)
+    PrimeFactors.of(4) shouldBe List(2,2)
+    PrimeFactors.of(6) shouldBe List(2,3)
+    PrimeFactors.of(8) shouldBe List(2,2,2)
+    PrimeFactors.of(9) shouldBe List(3,3)
+    PrimeFactors.of(25) shouldBe List(5,5)
+    PrimeFactors.of(49) shouldBe List(7,7)
+    PrimeFactors.of(2*2*7*11*13*17*17) shouldBe List(2,2,7,11,13,17,17)
+
   }
 }
